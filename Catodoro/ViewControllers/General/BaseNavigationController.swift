32405b76aa93ac72cce85a7ee8efe94766787254
Suguru Tokuda <suguru.tokuda@gmail.com>
2025-05-18 11:57:34 -0500

    Captured strong reference of self

--- Catodoro/ViewControllers/General/BaseNavigationController.swift
+++ Catodoro/ViewControllers/General/BaseNavigationController.swift
@@ -16,19 +16,19 @@ class BaseNavigationController: UINavigationController {
     }
 
     func contains(viewController: UIViewController) -> Bool {
-        return self.viewControllers.map { $0.className }.contains(viewController.className)
+        return viewControllers.map { $0.className }.contains(viewController.className)
     }
 
     override func pushViewController(_ viewController: UIViewController, animated: Bool) {
-        if (!self.isNewViewControllerBeingAdded && !self.contains(viewController: viewController)) {
-            self.isNewViewControllerBeingAdded = true
-            super.pushViewController(viewController, animated: true)
+        if (!isNewViewControllerBeingAdded && !contains(viewController: viewController)) {
+            isNewViewControllerBeingAdded = true
+            super.pushViewController(viewController, animated: animated)
         }
     }
 }
 
 extension BaseNavigationController: UINavigationControllerDelegate {
     func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
-        self.isNewViewControllerBeingAdded = false
+        isNewViewControllerBeingAdded = false
     }
 }
